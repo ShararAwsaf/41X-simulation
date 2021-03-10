e6e3f08d4328ca7b4dc2587426eeb436d4bdddf6
ShararAwsaf <sharar@uoguelph.ca>
2021-03-10 16:46:32 -0500

    Added code to detect images

--- object-detect-combined.py
+++ object-detect-combined.py
@@ -13,60 +13,82 @@ target_classes = segment_image.select_target_classes(person=True)
 toc = time.perf_counter()
 print(f"INITIALIZING TIME: {toc - tic:0.4f} seconds")
 
+def video_detect(): 
 
-#################### Setting up the file ################
+    #################### Setting up the file ################
 
-videoFile = "./detection/stctrim.mp4"
-vidcap = cv2.VideoCapture(videoFile)
-success,image = vidcap.read()
+    videoFile = "./detection/stctrim.mp4"
+    vidcap = cv2.VideoCapture(videoFile)
+    success,image = vidcap.read()
 
-#################### Setting up parameters ################
+    #################### Setting up parameters ################
 
-seconds = 2
-fps = vidcap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
-multiplier = round(fps * seconds)
+    seconds = 2
+    fps = vidcap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
+    multiplier = round(fps * seconds)
 
-#################### Initiate Process ################
-max_count = 0
-prev_max = 0
-avg = 0.0
+    #################### Initiate Process ################
+    max_count = 0
+    prev_max = 0
+    avg = 0.0
 
-while success:
-    frameId = int(round(vidcap.get(1))) #current frame number, rounded b/c sometimes you get frame intervals which aren't integers...this adds a little imprecision but is likely good enough
-    success, image = vidcap.read()
-    # print(frameId, multiplier)
-    
-    if frameId % multiplier == 0:
+    while success:
+        frameId = int(round(vidcap.get(1))) #current frame number, rounded b/c sometimes you get frame intervals which aren't integers...this adds a little imprecision but is likely good enough
+        success, image = vidcap.read()
+        # print(frameId, multiplier)
         
-        src_img = "./detection/frames/frame%d.jpg" % frameId
-        # cv2.imwrite(src_img, image)
+        if frameId % multiplier == 0:
+            
+            src_img = "./detection/frames/frame%d.jpg" % frameId
+            # cv2.imwrite(src_img, image)
 
-        ######################### DETECTION ################################
-        tic = time.perf_counter()
-        segmask, output = segment_image.segmentFrame(image, 
-                                show_bboxes = True, 
-                                segment_target_classes=target_classes, 
-                                extract_segmented_objects= False, 
-                                save_extracted_objects=False,
-                                output_image_name = "./detection/output/{}-person-only.jpg".format(frameId)
-                                )
-        toc = time.perf_counter()
+            ######################### DETECTION ################################
+            tic = time.perf_counter()
+            segmask, output = segment_image.segmentFrame(image, 
+                                    show_bboxes = True, 
+                                    segment_target_classes=target_classes, 
+                                    extract_segmented_objects= False, 
+                                    save_extracted_objects=False,
+                                    output_image_name = "./detection/output/{}-person-only.jpg".format(frameId)
+                                    )
+            toc = time.perf_counter()
 
-        bbox = len(segmask['rois'])
-        avg = (avg + bbox) // 2
-        print("{} BBOX COORDINATES:".format(frameId), bbox)
-        print("{} CLASS IDS:".format(frameId), len(segmask['class_ids']))
+            bbox = len(segmask['rois'])
+            avg = (avg + bbox) // 2
+            print("{} BBOX COORDINATES:".format(frameId), bbox)
+            print("{} CLASS IDS:".format(frameId), len(segmask['class_ids']))
 
-        print(f"{frameId} RUNNING AVG: {avg} ")
-        print(f"{frameId} TIME: {toc - tic:0.4f} seconds")
+            print(f"{frameId} RUNNING AVG: {avg} ")
+            print(f"{frameId} TIME: {toc - tic:0.4f} seconds")
 
-        max_count = max(max_count, bbox)
+            max_count = max(max_count, bbox)
 
-        if prev_max != max_count:
-            print("MAX COUNT CHANGED:", max_count)
-            prev_max = max_count            
+            if prev_max != max_count:
+                print("MAX COUNT CHANGED:", max_count)
+                prev_max = max_count            
 
-vidcap.release()
-print ("Complete")
-print("MAX COUNT:", max_count)
-print(f"RUNNING AVG: {avg} ")
+    vidcap.release()
+    print ("Complete")
+    print("MAX COUNT:", max_count)
+    print(f"RUNNING AVG: {avg} ")
+
+def image_detect():
+    path = "./detection/"
+    image = "russia-mall-1"
+    frmt = ".jpg"
+    tic = time.perf_counter()
+    segmask, output = segment_image.segmentImage(path+image+frmt, 
+                            show_bboxes = True, 
+                            segment_target_classes=target_classes, 
+                            extract_segmented_objects= False, 
+                            save_extracted_objects=False,
+                            output_image_name = "./detection/output/{}-person-only.jpg".format(image)
+                            )
+    toc = time.perf_counter()
+    bbox = len(segmask['rois'])
+
+    print("{} BBOX COORDINATES:".format(image), bbox)
+    print("{} CLASS IDS:".format(image), len(segmask['class_ids']))
+    print(f"{image} TIME: {toc - tic:0.4f} seconds")
+
+image_detect()
